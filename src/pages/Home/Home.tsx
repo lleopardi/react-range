709e065cea92321ca5e304030ed9f5a753885f08
Luis Leopardi <leopardi.lui@gmail.com>
2021-09-06 00:02:44 -0500

    style(home): add style

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            Demo de input range(Sin el input), solo consta de divs y Drag and Drop
+        <div className="home">
+            <h1>Demo de input range(Sin el input), solo consta de divs y Drag and Drop</h1>
         </div>
     );
 }
