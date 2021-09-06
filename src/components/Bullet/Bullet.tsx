49006f0b2c927bfbad3a05c43926043f280dab37
Luis Leopardi <leopardi.lui@gmail.com>
2021-09-06 10:14:18 -0500

    refactor(bullet): change DnD to mouse events

--- src/components/Bullet/Bullet.tsx
+++ src/components/Bullet/Bullet.tsx
@@ -1,34 +1,64 @@
 import * as React from 'react';
+import { useState } from 'react';
 import './Bullet.css';
 
-
+enum CursorStyle {
+    grabbing='grabbing',
+    grab='grab'
+}
 interface BulletProps {
-    onDragStart: (event: React.DragEvent<HTMLDivElement>) => void,
     onDrag: (event: React.DragEvent<HTMLDivElement>) => void,
     onDragEnd: (event: React.DragEvent<HTMLDivElement>) => void,
     position: number,
     id: 'min' | 'max'
 }
 
-const Bullet = ({ onDragStart, onDrag, onDragEnd, position, id }: BulletProps) => {
+const Bullet = ({ onDrag, onDragEnd, position, id }: BulletProps) => {
+    const [isPressed, setIsPressed] = useState(false);
+    const [cursorStyle, setCursorStyle] = useState('');
 
-    const dragStart = (event: React.DragEvent<HTMLDivElement>) => {
-        onDragStart(event);
+    const handlerOnMouseDown = (event: React.DragEvent<HTMLDivElement>) => {
+        setIsPressed(true);
+        setCursorStyle(CursorStyle.grabbing);
+        event.preventDefault();
     }
 
-    const drag = (event: React.DragEvent<HTMLDivElement>) => {
-        onDrag(event);
+    const handlerOnMouseMove = (event: React.DragEvent<HTMLDivElement>) => {
+        if(isPressed){
+            setCursorStyle(CursorStyle.grabbing);
+            onDrag(event);
+        }
+        event.preventDefault();
+    }
+
+    const handlerOnMouseUp = (event: React.DragEvent<HTMLDivElement>) => {
+        onDragEnd(event);
+        setIsPressed(false);
+        setCursorStyle(CursorStyle.grab);
+        event.preventDefault();
+    }
+
+    const handlerOnMouseEnter = (event: React.DragEvent<HTMLDivElement>) => {
+        setCursorStyle(CursorStyle.grab);
+        setIsPressed(false);
+        event.preventDefault();
+    }
+
+    const handlerOnMouseLeave = (event: React.DragEvent<HTMLDivElement>) => {
+        setCursorStyle('');
+        event.preventDefault();
     }
 
     return (
         <div
             id={id}
-            className={`bullet`}
+            className={`bullet ${cursorStyle}`}
             data-testid="bullet"
-            draggable
-            onDragStart={dragStart}
-            onDrag={drag}
-            onDragEnd={onDragEnd}
+            onMouseDown={handlerOnMouseDown}
+            onMouseMove={handlerOnMouseMove}
+            onMouseUp={handlerOnMouseUp}
+            onMouseEnter={handlerOnMouseEnter}
+            onMouseLeave={handlerOnMouseLeave}
             style={{
                 left: `${position}px`
             }}>
