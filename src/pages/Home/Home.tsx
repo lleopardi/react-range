2ad4075cc46bbcede21cf0ea8ec6cb84afaff631
Luis Leopardi <leopardi.lui@gmail.com>
2021-09-06 11:11:27 -0500

    chore(home): update text

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ import './Home.css';
 const Home = () => {
     return (
         <div className="home">
-            <h1>Demo de input range(Sin el input), solo consta de divs y Drag and Drop</h1>
+            <h1>Demo de input range(Sin el input), solo consta de divs</h1>
         </div>
     );
 }
